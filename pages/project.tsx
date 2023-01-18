276071780a493e9686a90499c2fd3e7d7c5e12f7
Ved Mishra <ved.mishra@outlook.com>
2023-01-17 22:39:36 -0500

    Add projects content

---
+++ pages/project.tsx
@@ -0,0 +1,41 @@
+import Layout from "./components/layout";
+import data from '../data/data.json';
+import SEO from "./components/seo";
+import ProjectCard from "./components/project-card";
+
+interface Props {
+  projects: [
+    { title: string; description: string; link: string }
+  ];
+}
+const Projects = ({ projects }: Props) => {
+  return (
+    <>
+      <SEO
+        title="Project"
+        description="This is the project page for my portfolio website"
+        image="https://example.com/image.jpg"
+      />
+      <Layout title="Projects">
+        <div className="bg-white p-4">
+          <h2 className="text-lg font-medium text-gray-800">Projects</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {projects && projects.map((project, index) => (
+              <ProjectCard key={index} project={project} />
+            ))}
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      projects: data.projects
+    },
+  }
+}
+
+export default Projects;
