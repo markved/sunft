36f5ac5c1dd62ced33db06f59a382abaf1a9e806
Ved Mishra <ved.mishra@outlook.com>
2023-01-31 19:02:10 -0500

    Added authentication layer

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { signIn, signOut, useSession } from "next-auth/react"
 import Head from "next/head";
 import { useRouter } from "next/router";
 import SocialMedia from "./social-media";
+import styles from "./header.module.css"
 
 interface Props {
   children: React.ReactNode;
@@ -12,6 +14,8 @@ const Layout = ({ children, title = "My App" }: Props) => {
   const router = useRouter();
   // set isMounted to false initially to load social media icons after page load
   const [isMounted, setIsMounted] = useState(false);
+  const { data: session, status } = useSession()
+  const loading = status === "loading"
   useEffect(() => {
     setIsMounted(true);
   }, []);
@@ -30,7 +34,53 @@ const Layout = ({ children, title = "My App" }: Props) => {
       <header className="bg-purple-400 p-4">
         <nav className="items-center justify-between md:flex">
           <div className="flex items-start">
-            {isMounted && <SocialMedia color={'white'} />}
+            <p
+              className={`nojs-show ${!session && loading ? styles.loading : styles.loaded
+                }`}
+            >
+              {!session && (
+                <>
+                  <span className={styles.notSignedInText}>
+                    You are not signed in
+                  </span>
+                  <a
+                    href={`/api/auth/signin`}
+                    className={styles.buttonPrimary}
+                    onClick={(e) => {
+                      e.preventDefault()
+                      signIn()
+                    }}
+                  >
+                    Sign in
+                  </a>
+                </>
+              )}
+              {session?.user && (
+                <>
+                  {session.user.image && (
+                    <span
+                      style={{ backgroundImage: `url('${session.user.image}')` }}
+                      className={styles.avatar}
+                    />
+                  )}
+                  <span className={styles.signedInText}>
+                    <small>Signed in as</small>
+                    <br />
+                    <strong>{session.user.email ?? session.user.name}</strong>
+                  </span>
+                  <a
+                    href={`/api/auth/signout`}
+                    className={styles.button}
+                    onClick={(e) => {
+                      e.preventDefault()
+                      signOut()
+                    }}
+                  >
+                    Sign out
+                  </a>
+                </>
+              )}
+            </p>
           </div>
           <div className="flex uppercase text-white items-center">
             <a href="#" className={`px-1 py-1 mr-1  ${router.pathname === '/' && 'underline underline-offset-8'}`} onClick={() => handleClick("/")}>Home</a>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
+import { SessionProvider } from "next-auth/react"
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from "next/app"
+import type { Session } from "next-auth"
+
+export default function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
-
-export default MyApp
